b9c40ddc2dbd910864eff6d1a7e610c03fdb4f87
ArthurBlasi <rthrblasi@gmail.com>
2024-07-18 16:51:59 -0300

    dando funcionalidades aos botoes numéricos.

--- src/aplicacao/ACMECalculator.java
+++ src/aplicacao/ACMECalculator.java
@@ -5,29 +5,134 @@ import java.util.ArrayList;
 public class ACMECalculator {
 
     private ArrayList<String> calculos;
+    private StringBuilder sb;
 
     public ACMECalculator() {
-        calculos = new ArrayList();
+        sb = new StringBuilder();
     }
 
-    public void limparLista() {
-        calculos.clear();
+    public void limpar() {
+        sb.setLength(0);
     }
 
     public void apagar() {
-        calculos.removeLast();
+        int length = sb.length();
+        sb.deleteCharAt(length - 1);
     }
 
-    public void addNum(String n) {
-        calculos.add(n);
+    public boolean addNum(char n) {
+        if(n == ',') {
+            for(int i = 0; i < sb.length(); i++) {
+                if(sb.charAt(i) == ',') {
+                    return false;
+                }
+            }
+        }
+        sb.append(n);
+        return  true;
     }
 
+    public void result() {
+        // Dividir a expressão no operador "+"
+        String[] partes = sb.toString().split("\\+");
 
+        // Converter as partes para inteiros
+        int numero1 = Integer.parseInt(partes[0]);
+        int numero2 = Integer.parseInt(partes[1]);
 
+        // Realizar a soma
+        int resultado = numero1 + numero2;
 
+        // Construir a expressão e o resultado
+        limpar();
+        sb.append(numero1).append("+").append(numero2).append("=").append(resultado);
 
-    public ArrayList<String> getCalculos() {
-        return calculos;
     }
-    
+
+    public StringBuilder getSb() {
+        return sb;
+    }
+
 }
+
+
+
+/*
+public static int avaliarExpressao(String expressao) {
+    // Pilhas para números e operadores
+    Stack<Integer> numeros = new Stack<>();
+    Stack<Character> operadores = new Stack<>();
+
+    for (int i = 0; i < expressao.length(); i++) {
+        char c = expressao.charAt(i);
+
+        // Ignorar espaços em branco
+        if (Character.isWhitespace(c)) {
+            continue;
+        }
+
+        // Se o caractere for um número, capturar o número completo
+        if (Character.isDigit(c)) {
+            StringBuilder sb = new StringBuilder();
+            while (i < expressao.length() && Character.isDigit(expressao.charAt(i))) {
+                sb.append(expressao.charAt(i++));
+            }
+            i--;
+            numeros.push(Integer.parseInt(sb.toString()));
+        } else if (c == '(') {
+            // Se for um parêntese de abertura, empilhar na pilha de operadores
+            operadores.push(c);
+        } else if (c == ')') {
+            // Se for um parêntese de fechamento, resolver toda a expressão dentro dos parênteses
+            while (operadores.peek() != '(') {
+                numeros.push(aplicarOperador(operadores.pop(), numeros.pop(), numeros.pop()));
+            }
+            operadores.pop();
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            // Se for um operador, considerar precedência
+            while (!operadores.isEmpty() && precedencia(c) <= precedencia(operadores.peek())) {
+                numeros.push(aplicarOperador(operadores.pop(), numeros.pop(), numeros.pop()));
+            }
+            operadores.push(c);
+        }
+    }
+
+    // Aplicar os operadores restantes na pilha
+    while (!operadores.isEmpty()) {
+        numeros.push(aplicarOperador(operadores.pop(), numeros.pop(), numeros.pop()));
+    }
+
+    // O resultado final estará na pilha de números
+    return numeros.pop();
+}
+
+public static int precedencia(char operador) {
+    switch (operador) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return -1;
+    }
+}
+
+public static int aplicarOperador(char operador, int b, int a) {
+    switch (operador) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0) {
+                throw new UnsupportedOperationException("Divisão por zero não é permitida");
+            }
+            return a / b;
+        default:
+            return 0;
+    }
+}*/
--- src/ui/telaCalculadora.java
+++ src/ui/telaCalculadora.java
@@ -45,7 +45,7 @@ public class telaCalculadora extends javax.swing.JDialog {
             @Override
             public void actionPerformed(ActionEvent e) {
                 textArea.setText("");
-                acme.limparLista();
+                acme.limpar();
             }
         });
 
@@ -60,21 +60,169 @@ public class telaCalculadora extends javax.swing.JDialog {
             @Override
             public void actionPerformed(ActionEvent e) {
                 acme.apagar();
-                textArea.setText(acme.getCalculos().toString());
+                textArea.setText(acme.getSb().toString());
             }
         });
 
         zeroButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                acme.addNum("0");
-                textArea.setText(acme.getCalculos().toString());
+                acme.addNum('0');
+                textArea.setText(acme.getSb().toString());
             }
         });
 
+        oneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('1');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
 
+        twoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('2');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
 
+        threeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('3');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
 
+        fourButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('4');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        fiveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('5');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        sixButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('6');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        sevenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('7');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        eightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('8');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        nineButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('9');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        virgButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum(',');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        sumButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.addNum('+');
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        subButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        xButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        divButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        equalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acme.result();
+                textArea.setText(acme.getSb().toString());
+            }
+        });
+
+        porcentagemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        rootButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        onebyxButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        potButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        mmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     @Override
